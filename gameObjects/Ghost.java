7f86f3e03293ba359cc2fab360b24e0d65201765
Cristian Sammarco <cristiansammarco2005@gmail.com>
2025-04-16 11:35:04 +0200

    Ghost tornano allo start

--- gameObjects/Ghost.java
+++ gameObjects/Ghost.java
@@ -3,21 +3,25 @@ package gameObjects;
 import abstracts.Drawable;
 import java.awt.*;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 import utils.Coordinates;
 import utils.Direction;
 import utils.DrawingInformation;
 
+
 public class Ghost extends Drawable {
 
     private ArrayList<Direction> availDirections; // salvare posizioni dei fantasmi
+    private static final List<Ghost> allGhosts = new ArrayList<>();
 
     Random random = new Random(); // random per il movimento dei fantasmi
     private Direction lastDir; // salvo l'ultima direzione del ghost
-
+    private final Coordinates initialDirection; // salvo le coordinate iniziali del ghost
     public Ghost(Coordinates coordinates) {
         super(coordinates);
-        // TODO: IMPLEMENT
+        this.initialDirection = new Coordinates(coordinates.getRow(), coordinates.getCol());
+        allGhosts.add(this);
     }
 
     public void setAvailDirections(ArrayList<Direction> availDirections) {
@@ -27,7 +31,7 @@ public class Ghost extends Drawable {
     @Override
     public void update() {
         if (availDirections != null && !availDirections.isEmpty()) {
-            // Scegli una direzione casuale DALLA LISTA delle direzioni disponibili
+            // Scegli una direzione casuale delle direzioni disponibili
             Direction randomDirection = availDirections.get(random.nextInt(availDirections.size()));
 
             // se ha piu strade, il randomDirection non deve essere == al suo opposto, cosi
@@ -41,9 +45,9 @@ public class Ghost extends Drawable {
             } else if (availDirections.size() == 1) {
 
                 /*
-                 * passo alla funzione move il random direction, che sarà uguale al suo opposto
-                 * (cosi puo tornare indietro).
-                 * La sua ultima posizione sarà quindi randomDirection.
+                passo alla funzione move il random direction, che sarà uguale al suo opposto
+                (cosi puo tornare indietro).
+                La sua ultima posizione sarà quindi randomDirection.
                  */
                 move(randomDirection);
                 this.lastDir = randomDirection;
@@ -70,7 +74,22 @@ public class Ghost extends Drawable {
 
     @Override
     public DrawingInformation draw() {
-        return new DrawingInformation('G', new Color(255, 60, 0));
+        return new DrawingInformation('G', new Color(255, 60, 0)); // Disegno i Ghost in mappa
+    }
+
+    public void resetCoordinates(){  
+        // Resetto le coordinate del ghost alla posizione iniziale
+        coordinates = new Coordinates(initialDirection.getRow(), initialDirection.getCol());
+        System.out.println("Ghost reset to initial position: " + initialDirection);
+        
+    }
+
+    public static void resetAllGhosts() {
+        // Entro in ogni ghost e resetto le sue coordinate
+        // alla posizione iniziale
+        for (Ghost g : allGhosts) {
+            g.resetCoordinates();
+        }
     }
 
 }
--- gameObjects/Player.java
+++ gameObjects/Player.java
@@ -6,32 +6,32 @@ import utils.Coordinates;
 import utils.Direction;
 import utils.DrawingInformation;
 
-
-
 public class Player extends Controllable {
 
     private static final int INITIAL_LIFE = 3; // Costanti
 
-    //controllo se c'è un muro
+    // controllo se c'è un muro
     public int currentRow = coordinates.getRow();
     public int currentCol = coordinates.getCol();
 
-    //varibaili per tener conto dell'ultima posizione
+    // varibaili per tener conto dell'ultima posizione
     public int lastRow;
     public int lastCol;
 
-    //variabile per tenere in memoria la posizione iniziale
+    // variabile per tenere in memoria la posizione iniziale
     private final Coordinates startCoords;
 
-    private Direction currentDirection; //direzione corrente
-    
-    private int life; //variabile per tener conto delle vite
+    private Direction currentDirection; // direzione corrente
 
-    private boolean winner = false; //variabile da ritornare per sapere se la partita è vinta. False = no; True = WIN!
+    private int life; // variabile per tener conto delle vite
 
-    public char lastKey = 'p'; //char per settare l'ultimo tasto cliccato: P = stay; W = UP; S = DOWN; D = RIGT; S = LEFT
+    private boolean winner = false; // variabile da ritornare per sapere se la partita è vinta. False = no; True =
+                                    // WIN!
 
-    private char previousKey = 'p'; // Memorizza il tasto precedente    
+    public char lastKey = 'p'; // char per settare l'ultimo tasto cliccato: P = stay; W = UP; S = DOWN; D =
+                               // RIGT; S = LEFT
+
+    private char previousKey = 'p'; // Memorizza il tasto precedente
 
     public Player(Coordinates coordinates) {
         super(coordinates);
@@ -74,65 +74,66 @@ public class Player extends Controllable {
                 lastKey = 'a';
             }
             case STAY -> stayMovement();
-            default -> {}
+            default -> {
+            }
         }
     }
 
     public boolean isWinner() {
-        return this.winner; //ritorniamo winner. Se false, si prosegue il gioco, se true la partita è vinta
+        return this.winner; // ritorniamo winner. Se false, si prosegue il gioco, se true la partita è vinta
     }
 
     public boolean isLooser() {
-        return getLife() == 0; //se le vite sono = 0, la partita è persa
+        return getLife() == 0; // se le vite sono = 0, la partita è persa
     }
 
     public void markAsWinner() {
-        this.winner = true; //nel caso in cui si vinca, si setta winner a true
+        this.winner = true; // nel caso in cui si vinca, si setta winner a true
     }
 
     public void handleDamage() {
-        //in caso si viene mangiati, si ritorna alle coordinate iniziali, si sta fermi e si perde una vita
+        // in caso si viene mangiati, si ritorna alle coordinate iniziali, si sta fermi
+        // e si perde una vita
         coordinates = new Coordinates(startCoords.getRow(), startCoords.getCol());
         lastKey = 'p';
         life--;
+
+        Ghost.resetAllGhosts(); //chiamo ghost per resettare la loro posizione
     }
 
     @Override
     public DrawingInformation draw() {
-        //settiamo il colore per il player
-        return new DrawingInformation('P', new Color	(255, 242, 0));
+        // settiamo il colore per il player
+        return new DrawingInformation('P', new Color(255, 242, 0));
     }
 
     public void resetCoordinates() {
-        //System.out.println("last1: " + lastKey);
-        //System.out.println("previous1: " + previousKey);
+        coordinates = new Coordinates(lastRow, lastCol); // resetto coordinate quando arrivo a un muro
 
-        coordinates = new Coordinates(lastRow, lastCol); //resetto coordinate quando arrivo a un muro
-
-        //System.out.println("last: " + lastKey);
-        //System.out.println("previous: " + previousKey);
-
-        if (lastKey!=previousKey) {
-            lastKey = previousKey; //resetto l'ultimo tasto premuto
-            stayMovement(); //porto direttamente a stayMovement cosi non ho problemi di lag
-        }else{
-            lastKey = 'p'; //se non è cambiato il tasto, lo setto a p
+        if (lastKey != previousKey) {
+            lastKey = previousKey; // resetto l'ultimo tasto premuto
+            stayMovement(); // porto direttamente a stayMovement cosi non ho problemi di lag
+        } else {
+            lastKey = 'p'; // se non è cambiato il tasto, lo setto a p
         }
     }
 
     public int getLife() {
-        return life; //funzione per ritornare il numero delle vite
+        return life; // funzione per ritornare il numero delle vite
     }
 
-    public void stayMovement(){
-        //nel caso non venga premuto alcun tasto, si utilizza il vecchio tasto per continuare il movimento
-        switch(lastKey){
-            case 'w' -> coordinates = new Coordinates(currentRow -1, currentCol);
-            case 's' -> coordinates = new Coordinates(currentRow +1, currentCol);
-            case 'd' -> coordinates = new Coordinates(currentRow, currentCol +1); 
-            case 'a' -> coordinates = new Coordinates(currentRow, currentCol -1);
-            case 'p' ->  {}
-            default -> {}       
+    public void stayMovement() {
+        // nel caso non venga premuto alcun tasto, si utilizza il vecchio tasto per
+        // continuare il movimento
+        switch (lastKey) {
+            case 'w' -> coordinates = new Coordinates(currentRow - 1, currentCol);
+            case 's' -> coordinates = new Coordinates(currentRow + 1, currentCol);
+            case 'd' -> coordinates = new Coordinates(currentRow, currentCol + 1);
+            case 'a' -> coordinates = new Coordinates(currentRow, currentCol - 1);
+            case 'p' -> {
+            }
+            default -> {
+            }
         }
     }
 }
